3ec8785931ad26984d863089250f1196bbabb49b
Mischmschine <77553282+Mischmschine@users.noreply.github.com>
2022-05-27 01:00:27 +0200

    Updated build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,13 +17,16 @@ dependencies {
     implementation("org.mongodb:mongodb-driver-sync:4.6.0")
     implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("com.google.code.gson:gson:2.9.0")
+
     implementation("mysql:mysql-connector-java:8.0.29")
+    implementation("org.postgresql:postgresql:42.3.4")
+
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
 }
 
-tasks.test {
+/*tasks.test {
     useJUnitPlatform()
-}
+}*/
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
