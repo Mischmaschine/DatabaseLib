1a8c61c94919eb930e5ea804aab4d5e549982379
Mischmschine <77553282+Mischmschine@users.noreply.github.com>
2022-06-20 23:17:05 +0200

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.6.21"
     id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("org.jetbrains.dokka") version "1.6.21"
 }
 
 group = "de.mischmaschine"
@@ -15,7 +16,6 @@ repositories {
 }
 
 dependencies {
-    implementation("org.mongodb:mongodb-driver-sync:4.6.0")
     implementation("com.zaxxer:HikariCP:5.0.1")
 
     implementation("mysql:mysql-connector-java:8.0.29")
@@ -24,9 +24,12 @@ dependencies {
     implementation("org.xerial:sqlite-jdbc:3.36.0.3")
     implementation("org.mariadb.jdbc:mariadb-java-client:3.0.4")
     implementation("org.mongodb:mongodb-driver-sync:4.6.0")
+    implementation("org.mongodb:mongodb-driver-sync:4.6.0")
 
     implementation("com.google.code.gson:gson:2.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.6.21")
+    //implementation("com.esotericsoftware:reflectasm:1.11.9")
 }
 
 tasks {
@@ -34,9 +37,9 @@ tasks {
         dependsOn(shadowJar)
     }
 }
-/*tasks.test {
+tasks.test {
     useJUnitPlatform()
-}*/
+}
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
