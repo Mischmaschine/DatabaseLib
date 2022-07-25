19306eca5654d0ef4e7aee5f5816cb11c7a5777a
Mischmaschine <fixxertrixxer@gmail.com>
2022-07-25 18:40:38 +0200

    feat: add pubSub System, change redis api

--- src/main/kotlin/de/mischmaschine/database/redis/AbstractRedis.kt
+++ src/main/kotlin/de/mischmaschine/database/redis/AbstractRedis.kt
@@ -3,76 +3,88 @@ package de.mischmaschine.database.redis
 import com.google.gson.GsonBuilder
 import de.mischmaschine.database.database.Configuration
 import de.mischmaschine.database.database.Database
-import org.redisson.Redisson
-import org.redisson.api.RFuture
-import org.redisson.api.RedissonClient
-import org.redisson.codec.JsonJacksonCodec
-import org.redisson.config.Config
+import io.lettuce.core.RedisClient
+import io.lettuce.core.RedisFuture
+import io.lettuce.core.RedisURI
+import io.lettuce.core.api.StatefulRedisConnection
+import io.lettuce.core.api.async.RedisAsyncCommands
+import io.lettuce.core.api.sync.RedisCommands
+import io.lettuce.core.pubsub.RedisPubSubAdapter
+import io.lettuce.core.pubsub.StatefulRedisPubSubConnection
 
-abstract class AbstractRedis(private val database: Int) : Database {
 
-    private val client: RedissonClient
+abstract class AbstractRedis(database: Int) : Database {
+
+    private val client: RedisClient
+    private val connection: StatefulRedisConnection<String, String>
     private val gson = GsonBuilder().serializeNulls().create()
+    private val redisSync: RedisCommands<String, String>
+    private val redisAsync: RedisAsyncCommands<String, String>
+    private val executors = mutableMapOf<String, (String, String) -> Unit>()
+    private val pubSub: StatefulRedisPubSubConnection<String, String>
 
     init {
-        val config = Config().apply {
-            codec = JsonJacksonCodec()
-            useSingleServer().apply {
-                address =
-                    "redis://" + Configuration.getHost(AbstractRedis::class) + ":" + Configuration.getPort(AbstractRedis::class)
-                idleConnectionTimeout = 10000
-                timeout = 3000
-                retryInterval = 1000
-                dnsMonitoringInterval = 5000
-                subscriptionsPerConnection = 2
-                subscriptionConnectionPoolSize = 25
-                subscriptionConnectionMinimumIdleSize = 1
-                password = Configuration.getPassword(AbstractRedis::class)
-                database = this@AbstractRedis.database
-            }
-        }
-        this.client = Redisson.create(config);
+
+        val host = Configuration.getHost(AbstractRedis::class)
+        val port = Configuration.getPort(AbstractRedis::class)
+        val password = Configuration.getPassword(AbstractRedis::class)
+
+        this.client = RedisClient.create(
+            RedisURI.Builder.redis(
+                host,
+                port
+            ).withPassword(password.toCharArray()).withDatabase(database).build()
+        )
+        this.connection = client.connect()
+        this.redisAsync = connection.async()
+        this.redisSync = connection.sync()
+        this.pubSub = client.connectPubSub().also { it.addListener(Listener()) }
     }
 
     fun updateKeyAsync(key: String, data: Any) {
-        val rBucket = getClient().getBucket<String>(key)
-        rBucket.setAsync(gson.toJson(data))
+        redisAsync.set(key, gson.toJson(data))
     }
 
     fun updateKeySync(key: String, data: Any) {
-        val rBucket = getClient().getBucket<String>(key)
-        rBucket.set(gson.toJson(data))
+        redisSync.set(key, gson.toJson(data))
     }
 
-    fun getKeySync(key: String): String {
-        val rBucket = getClient().getBucket<String>(key)
-        return rBucket.get()
+    fun getValueSync(key: String): String? = redisSync.get(key)
+
+    fun getValueAsync(key: String): RedisFuture<String?> = redisAsync.get(key)
+
+    fun deleteKeySync(vararg key: String) {
+        redisSync.del(*key)
     }
 
-    fun getKeyAsync(key: String): RFuture<String> {
-        val rBucket = getClient().getBucket<String>(key)
-        return rBucket.async
+    fun deleteKeyAsync(vararg key: String) {
+        redisAsync.del(*key)
     }
 
-    fun deleteKeySync(key: String) {
-        getClient().getBucket<String>(key).delete()
+    fun subscribe(channel: String, executor: (String, String) -> Unit) {
+        pubSub.async().subscribe(channel)
+        executors[channel] = executor
     }
 
-    fun deleteKeyAsync(key: String) {
-        getClient().getBucket<String>(key).deleteAsync()
+    fun unSubScribe(vararg channel: String) {
+        pubSub.async().unsubscribe(*channel)
+        println("Unsubscribed from ${channel.joinToString(", ")}")
     }
 
-    fun existsSync(key: String): Boolean {
-        return getClient().getBucket<String>(key).isExists
+    fun publish(channel: String, message: String) {
+        client.connectPubSub().async().publish(channel, message)
+        println("Published to $channel: $message")
     }
 
-    fun existsAsync(key: String): RFuture<Boolean> {
-        return getClient().getBucket<String>(key).isExistsAsync
-    }
+    fun getAsyncClient() = connection.async()
+    fun getSyncClient() = connection.sync()
 
-    fun getBucket(key: String) = getClient().getBucket<String>(key)
+    inner class Listener : RedisPubSubAdapter<String, String>() {
 
-    fun getClient(): RedissonClient {
-        return client
+        override fun message(channel: String, message: String) =
+            executors[channel]?.invoke(channel, message) ?: let {
+                println("There is no executor for channel $channel. Channel $channel will be ignored.")
+                unSubScribe(channel)
+            }
     }
 }
