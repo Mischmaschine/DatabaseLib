bb9fb7458be3667ab899768a0f28e9c4f414dd58
Mischmaschine <fixxertrixxer@gmail.com>
2022-08-04 23:43:33 +0200

    chore: upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
 
     compileOnly("mysql:mysql-connector-java:8.0.29")
     compileOnly("org.postgresql:postgresql:42.3.6")
-    compileOnly("com.h2database:h2:2.1.212")
+    compileOnly("com.h2database:h2:2.1.214")
     compileOnly("org.xerial:sqlite-jdbc:3.36.0.3")
     compileOnly("org.mariadb.jdbc:mariadb-java-client:3.0.5")
     compileOnly("org.mongodb:mongodb-driver-sync:4.6.0")
@@ -30,7 +30,7 @@ dependencies {
     testImplementation("org.mongodb:mongodb-driver-sync:4.6.0")
 
     implementation("com.google.code.gson:gson:2.9.0")
-    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.6.21")
+    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.7.10")
     implementation(kotlin("reflect"))
 }
 
