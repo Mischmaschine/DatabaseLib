b9306f942d57df100a4cd49e699af8c8f1e01c37
Mischmaschine <fixxertrixxer@gmail.com>
2022-08-05 13:20:39 +0200

    chore: change codec to provide a better looking json

--- src/main/kotlin/de/mischmaschine/database/redis/AbstractRedis.kt
+++ src/main/kotlin/de/mischmaschine/database/redis/AbstractRedis.kt
@@ -6,6 +6,7 @@ import org.redisson.Redisson
 import org.redisson.api.RFuture
 import org.redisson.api.RedissonClient
 import org.redisson.codec.JsonJacksonCodec
+import org.redisson.codec.TypedJsonJacksonCodec
 import org.redisson.config.Config
 import java.util.logging.Level
 
@@ -47,8 +48,8 @@ abstract class AbstractRedis(database: Int, logging: Boolean, ssl: Boolean) : Da
      * @param key The key to update.
      * @param data The data to update the key with.
      */
-    fun <T> updateKeyAsync(key: String, data: T) {
-        this.redissonClient.getBucket<T>(key, JsonJacksonCodec()).setAsync(data)
+    inline fun <reified T : Any> updateKeyAsync(key: String, data: T) {
+        getRedissonClient().getBucket<T>(key, TypedJsonJacksonCodec(T::class.java)).setAsync(data)
     }
 
     /**
@@ -57,8 +58,8 @@ abstract class AbstractRedis(database: Int, logging: Boolean, ssl: Boolean) : Da
      * @param key The key to update.
      * @param data The data to update the key with.
      */
-    fun <T> updateKeySync(key: String, data: T) {
-        this.redissonClient.getBucket<T>(key, JsonJacksonCodec()).set(data)
+    inline fun <reified T> updateKeySync(key: String, data: T) {
+        getRedissonClient().getBucket<T>(key, TypedJsonJacksonCodec(T::class.java)).set(data)
     }
 
     /**
@@ -68,8 +69,8 @@ abstract class AbstractRedis(database: Int, logging: Boolean, ssl: Boolean) : Da
      *
      * @return the value of the given key, or null if the key does not exist.
      */
-    fun <T> getValueSync(key: String): RFuture<T?> =
-        this.redissonClient.getBucket<T>(key, JsonJacksonCodec()).async
+    inline fun <reified T> getValueSync(key: String): T? =
+        getRedissonClient().getBucket<T>(key, TypedJsonJacksonCodec(T::class.java)).get()
 
     /**
      * Gets the value of the given key asynchronously.
@@ -78,17 +79,17 @@ abstract class AbstractRedis(database: Int, logging: Boolean, ssl: Boolean) : Da
      *
      * @return the value of the given key, or null if the key does not exist.
      */
-    fun <T> getValueAsync(key: String): RFuture<T?> =
-        this.redissonClient.getBucket<T>(key, JsonJacksonCodec()).async
+    inline fun <reified T> getValueAsync(key: String): RFuture<T?> =
+        getRedissonClient().getBucket<T>(key, TypedJsonJacksonCodec(T::class.java)).async
 
     /**
      * Deletes the given key synchronously.
      * @param key The key to delete.
      * @see [redisSync]
      */
-    fun <T> deleteKeySync(vararg key: String) {
+    inline fun <reified T> deleteKeySync(vararg key: String) {
         key.forEach {
-            this.redissonClient.getBucket<T>(it, JsonJacksonCodec()).delete()
+            getRedissonClient().getBucket<T>(it, TypedJsonJacksonCodec(T::class.java)).delete()
         }
     }
 
@@ -98,9 +99,9 @@ abstract class AbstractRedis(database: Int, logging: Boolean, ssl: Boolean) : Da
      *
      * @see [redisAsync]
      */
-    fun <T> deleteKeyAsync(vararg key: String) {
+    inline fun <reified T> deleteKeyAsync(vararg key: String) {
         key.forEach {
-            this.redissonClient.getBucket<T>(it, JsonJacksonCodec()).deleteAsync()
+            getRedissonClient().getBucket<T>(it, TypedJsonJacksonCodec(T::class.java)).deleteAsync()
         }
     }
 
@@ -121,7 +122,7 @@ abstract class AbstractRedis(database: Int, logging: Boolean, ssl: Boolean) : Da
      */
     fun unsubscribe(vararg channel: String) {
         channel.forEach {
-            this.redissonClient.getTopic(it, JsonJacksonCodec()).removeAllListeners()
+            getRedissonClient().getTopic(it, JsonJacksonCodec()).removeAllListeners()
         }
         logger.info("Unsubscribed from ${channel.joinToString(", ")}")
     }
@@ -132,8 +133,8 @@ abstract class AbstractRedis(database: Int, logging: Boolean, ssl: Boolean) : Da
      * @param channel The channel to publish to.
      * @param message The message to publish.
      */
-    fun publish(channel: String, message: Any) {
-        this.redissonClient.getTopic(channel, JsonJacksonCodec()).publishAsync(message)
+    inline fun <reified T> publish(channel: String, message: T) {
+        getRedissonClient().getTopic(channel, TypedJsonJacksonCodec(T::class.java)).publishAsync(message)
         logger.info("Published to channel '$channel': '$message'")
     }
 
@@ -141,7 +142,7 @@ abstract class AbstractRedis(database: Int, logging: Boolean, ssl: Boolean) : Da
 
     private inner class Listener(channel: String, type: Class<*>, function: (String, Any) -> Unit) {
         init {
-            this@AbstractRedis.redissonClient.getTopic(channel, JsonJacksonCodec())
+            this@AbstractRedis.redissonClient.getTopic(channel, TypedJsonJacksonCodec(type))
                 .addListenerAsync(type) { _, message ->
                     function.invoke(channel, message)
                 }
