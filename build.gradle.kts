2500e0b72c7eadd86f8a53913df095054d2e000a
Mischmaschine <fixxertrixxer@gmail.com>
2022-12-27 14:51:35 +0100

    chore: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,22 +20,22 @@ dependencies {
     compileOnly("com.zaxxer:HikariCP:5.0.1")
 
     compileOnly("mysql:mysql-connector-java:8.0.30")
-    compileOnly("org.postgresql:postgresql:42.5.0")
+    compileOnly("org.postgresql:postgresql:42.5.1")
     compileOnly("com.h2database:h2:2.1.214")
-    compileOnly("org.xerial:sqlite-jdbc:3.39.2.1")
+    compileOnly("org.xerial:sqlite-jdbc:3.40.0.0")
     compileOnly("org.mariadb.jdbc:mariadb-java-client:3.0.6")
-    compileOnly("org.mongodb:mongodb-driver-sync:4.7.1")
-    compileOnly("io.lettuce:lettuce-core:6.2.0.RELEASE")
+    compileOnly("org.mongodb:mongodb-driver-sync:4.8.1")
+    compileOnly("io.lettuce:lettuce-core:6.2.2.RELEASE")
 
     testImplementation("mysql:mysql-connector-java:8.0.30")
     testImplementation("com.zaxxer:HikariCP:5.0.1")
-    testImplementation("io.lettuce:lettuce-core:6.2.0.RELEASE")
-    testImplementation("org.mongodb:mongodb-driver-sync:4.7.1")
+    testImplementation("io.lettuce:lettuce-core:6.2.2.RELEASE")
+    testImplementation("org.mongodb:mongodb-driver-sync:4.8.1")
     testImplementation("org.mariadb.jdbc:mariadb-java-client:3.0.6")
     
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
 
-    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.7.10")
+    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.7.20")
     implementation(kotlin("reflect"))
 }
 
@@ -54,7 +54,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "de.mischmaschine"
             artifactId = "DatabaseLib"
-            version = "1.1"
+            version = "1.2"
 
             from(components["java"])
         }
